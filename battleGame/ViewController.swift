0c60e2c3e509efca76aa8a4b1faf237c54f90920
Dreunlimited <dre4311@gmail.com>
2015-12-11 12:17:14 -0500

    Initial Commit

---
+++ battleGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  battleGame
+//
+//  Created by D'Andre Ealy on 12/11/15.
+//  Copyright © 2015 D'Andre Ealy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
